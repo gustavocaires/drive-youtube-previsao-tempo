68235455ce15534dd23d255619841da9b4edb0d0
Gustavo Caires <gustavocaires@hotmail.com.br>
2024-09-23 19:55:31 -0300

    Estrutura e base do front

---
+++ index.js
@@ -0,0 +1,6 @@
+const API_KEY = '47df93b82bb65312e6483094293aaca9'
+
+function getData(local) {
+  const route = `https://api.openweathermap.org/data/2.5/weather?q=${local}&lang=pt_br&units=metric&appid=${API_KEY}`
+  return fetch(route).then(response => response.json())
+}
